0e0ba636754b30e923e6b7a1aa86e7f7dab198fb
ithanlp <elpezpardo@gmail.com>
2018-11-13 09:39:10 +0100

    nuevas ventanas listados

---
+++ app/src/main/java/restaurantguide/cifprodolfoucha/com/restaurantguide/Listado_Favoritos.java
@@ -0,0 +1,13 @@
+package restaurantguide.cifprodolfoucha.com.restaurantguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Listado_Favoritos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado__favoritos);
+    }
+}
