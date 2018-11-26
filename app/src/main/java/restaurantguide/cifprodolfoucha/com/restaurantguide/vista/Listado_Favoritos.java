82e384da520e0f1916e42c44a7230969b115b211
ithanlp <elpezpardo@gmail.com>
2018-11-26 11:20:44 +0100

    Guardado de imagen en memoria

--- app/src/main/java/restaurantguide/cifprodolfoucha/com/restaurantguide/vista/Listado_Favoritos.java
+++ app/src/main/java/restaurantguide/cifprodolfoucha/com/restaurantguide/vista/Listado_Favoritos.java
@@ -1,16 +1,24 @@
 package restaurantguide.cifprodolfoucha.com.restaurantguide.vista;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.lang.reflect.GenericArrayType;
+import java.util.ArrayList;
 
 import restaurantguide.cifprodolfoucha.com.restaurantguide.R;
 import restaurantguide.cifprodolfoucha.com.restaurantguide.modelo.ImageAdapter;
 
 public class Listado_Favoritos extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +28,17 @@ public class Listado_Favoritos extends AppCompatActivity {
         GridView gv = findViewById(R.id.gvFavs);
         gv.setAdapter(new ImageAdapter(this));
 
+        String ruta = "";
+
+        final String finalRuta = ruta;
         gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(),Info_Imagen.class);
+
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.parse("file://" + finalRuta), "image/*");
                 startActivity(intent);
             }
         });
--- app/src/main/java/restaurantguide/cifprodolfoucha/com/restaurantguide/vista/Nueva_Entrada.java
+++ app/src/main/java/restaurantguide/cifprodolfoucha/com/restaurantguide/vista/Nueva_Entrada.java
@@ -1,12 +1,16 @@
 package restaurantguide.cifprodolfoucha.com.restaurantguide.vista;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.text.format.Time;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,38 +20,51 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 import restaurantguide.cifprodolfoucha.com.restaurantguide.R;
 
 public class Nueva_Entrada extends Activity implements OnClickListener {
 
-    ImageView img;
-    Spinner sp;
-    Button btn;
-    boolean control = false;
-    EditText etNombrePlato;
-    EditText etNombreRestaurante;
-    String nombrePlato;
-    EditText etDescripcion;
-    String descripcion;
-    CheckBox chkFav;
-    RadioButton rbtnCercanos;
-    RadioButton rbtnGuardados;
-    RadioButton rbtnNuevo;
+    private ImageView img;
+    private Spinner sp;
+    private Button btn;
+    private final int cons = 1;
+    private boolean control = false;
+    private EditText etNombrePlato;
+    private EditText etNombreRestaurante;
+    private String nombrePlato;
+    private EditText etDescripcion;
+    private String descripcion;
+    private CheckBox chkFav;
+    private RadioButton rbtnCercanos;
+    private RadioButton rbtnGuardados;
+    private RadioButton rbtnNuevo;
     public static ArrayList<String> imags = new ArrayList<>();
-    Bitmap bit;
+    private Bitmap bit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Acceso concedido", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "DENIED", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, cons);
+            Toast.makeText(this, "DENIED 2", Toast.LENGTH_SHORT).show();
+        }
+
+
         setContentView(R.layout.activity_nueva__entrada);
         sp = findViewById(R.id.spRestDelPlato);
         btn = findViewById(R.id.btnAceptar);
         img = findViewById(R.id.ivPlato);
-        chkFav=findViewById(R.id.chkFav);
+        chkFav = findViewById(R.id.chkFav);
         etNombrePlato = findViewById(R.id.etNombrePlato);
         etNombreRestaurante = findViewById(R.id.txtNuevoRestaurante);
         rbtnCercanos = findViewById(R.id.rbtnCercanos);
@@ -103,15 +120,38 @@ public class Nueva_Entrada extends Activity implements OnClickListener {
         //foto.putExtra(MediaStore.EXTRA_OUTPUT, uri);
 
         startActivityForResult(foto, 7777);
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 7777 && resultCode == RESULT_OK) {
+            OutputStream out;
+
             bit = (Bitmap) data.getExtras().get("data");
             System.out.println(bit.toString());
-
             img.setImageBitmap(bit);
+
+            File filepath = Environment.getExternalStorageDirectory();
+            File dir = new File(filepath.getAbsolutePath() + "/RestoGuide");
+            dir.mkdirs();
+
+            Time now = new Time();
+            now.setToNow();
+
+            File file = new File(dir, etNombrePlato.getText().toString().trim()+"-RG-"+now.toString()+".png");
+
+            Toast.makeText(this, "Guardada en principio", Toast.LENGTH_SHORT).show();
+
+            try {
+                out = new FileOutputStream(file);
+                bit.compress(Bitmap.CompressFormat.PNG, 100, out);
+                out.flush();
+                out.close();
+            } catch (Exception e) {
+                Toast.makeText(this, "Ha petao", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
@@ -122,10 +162,10 @@ public class Nueva_Entrada extends Activity implements OnClickListener {
         outState.putParcelable("bitmap", bit);
         outState.putString("plato", etNombrePlato.getText().toString());
         outState.putString("desc", etDescripcion.getText().toString());
-        outState.putBoolean("fav",chkFav.isChecked());
-        outState.putBoolean("cercano",rbtnCercanos.isChecked());
-        outState.putBoolean("guardado",rbtnGuardados.isChecked());
-        outState.putBoolean("nuevo",rbtnNuevo.isChecked());
+        outState.putBoolean("fav", chkFav.isChecked());
+        outState.putBoolean("cercano", rbtnCercanos.isChecked());
+        outState.putBoolean("guardado", rbtnGuardados.isChecked());
+        outState.putBoolean("nuevo", rbtnNuevo.isChecked());
 
     }
 
@@ -139,17 +179,17 @@ public class Nueva_Entrada extends Activity implements OnClickListener {
         etNombrePlato.setText(nombrePlato);
         descripcion = savedInstanceState.getString("desc");
         etDescripcion.setText(descripcion);
-        boolean fav=savedInstanceState.getBoolean("fav");
-        if (fav){
+        boolean fav = savedInstanceState.getBoolean("fav");
+        if (fav) {
             chkFav.setChecked(true);
         }
-        boolean cercano=savedInstanceState.getBoolean("cercano");
-        boolean guardado=savedInstanceState.getBoolean("guardado");
-        if (cercano){
+        boolean cercano = savedInstanceState.getBoolean("cercano");
+        boolean guardado = savedInstanceState.getBoolean("guardado");
+        if (cercano) {
             rbtnCercanos.setChecked(true);
-        }else if(guardado){
+        } else if (guardado) {
             rbtnCercanos.setChecked(true);
-        }else{
+        } else {
             rbtnNuevo.setChecked(true);
         }
 
